49873105ba9597ad995e780a29e3dc0c38d3e040
william-jiang-lai <wjiang@indocresearch.org>
2022-02-09 15:29:50 -0500

    fix webpack modules

--- config-overrides.js
+++ config-overrides.js
@@ -15,6 +15,29 @@ module.exports = {
         os: require.resolve("os-browserify"),
       },
     };
+    config["module"] = {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    };
     return config;
   },
 };
