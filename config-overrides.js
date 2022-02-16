585e1f1e9c2e1bc686cd62f3a054f4d491af7ce0
william-jiang-lai <wjiang@indocresearch.org>
2022-02-16 09:44:14 -0500

    Add scss loader

--- config-overrides.js
+++ config-overrides.js
@@ -29,6 +29,10 @@ module.exports = {
           use: ["style-loader", "css-loader"],
         },
         {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
           test: /\.(png|jpe?g|gif)$/i,
           use: [
             {
